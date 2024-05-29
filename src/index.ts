e483e1c9a95e63565e7a1ae0ba795454e287d210
cameron lee <cameron.lee@smus.ca>
2024-05-29 12:01:16 -0700

    skeleton file for apollo + graphql

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import {ApolloServer} from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+import pkg from 'pg';
+import 'dotenv/config';
+
+const {Pool} = pkg;
+interface User{
+    name: String,
+    id: number
+}
+
+const typeDefs = `#graphql
+    type User{
+        name: String
+        id: Int
+    }
+
+    type Query{
+        users: [User]
+    }
+`;
+const mockUserdata = [
+    {
+        name: "Jay",
+        id: 0
+    },
+    {
+        name:"Erik",
+        id: 1
+    }
+]
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+const resolvers = {
+    Query: {
+        users: getUsers
+    }
+}
+
+function getUsers(): User[]{
+    return mockUserdata;
+}
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  // Passing an ApolloServer instance to the `startStandaloneServer` function:
+  //  1. creates an Express app
+  //  2. installs your ApolloServer instance as middleware
+  //  3. prepares your app to handle incoming requests
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  
+  console.log(`🚀  Server ready at: ${url}`);
