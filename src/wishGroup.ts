4aedbd0a12d78e2067680b71e32c79c420a79cce
Julia Zhong <115321977+jooleyaz@users.noreply.github.com>
2024-06-11 22:20:33 -0700

    Added post and delete for wishgroup (#10)
    * edited editWish function
    
    * added wishgroup scaffolding
    
    * added create and delete for wishgroups
    
    ---------
    
    Co-authored-by: Kangbin (Jay) Park <jaybinpark@gmail.com>

--- src/wishGroup.ts
+++ src/wishGroup.ts
@@ -1,22 +1,33 @@
 import Pool from '../config/dbConnect';
 
 interface WishGroup {
-    groupid: String
+delete
     title: String
     houseid: String
+    color: String
+
 };
 
 
 export const typeDefs = `#graphql
+delete
+    input WishGroupInput{
+        title: String!
+        houseid: String!
+        color: String!
+    }
+    input WishGroupDelete{
+        title: String!
+        housid: String!
+    }
     type WishGroup{
-        groupid: String
         title: String
         houseid: String
+        color: String
     }
     type Mutation {
-    
-    }
-    type Query{
+        createWishGroup(wishgroup: WishGroupInput!): WishGroup
+        deleteWishGroup(title: String, houseid: String): WishGroup
 
     }
 `;
@@ -25,5 +36,42 @@ export const typeDefs = `#graphql
 
 
 export const resolvers = {
-    
+delete
+    Mutation: {
+        createWishGroup: async (_: any, { wishgroup }: any) => {
+            return await CreateWishGroup(wishgroup);
+        },
+        deleteWishGroup: async (_: any, { title, houseid }: any) => {
+            return await DeleteWishGroup(title, houseid);
+        }
+    },
+};
+
+async function CreateWishGroup(wishgroup: WishGroup) {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query('INSERT INTO wishgroup (title, houseid, color) VALUES($1, $2, $3) RETURNING *',
+            [wishgroup.title, wishgroup.houseid, wishgroup.color]);
+        return result.rows[0];
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+};
+
+async function DeleteWishGroup(title: String, houseid: String) {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query('DELETE FROM wishgroup WHERE title = $1 AND houseid = $2', [title, houseid]);
+        return {
+            success: true,
+            message: "WishGroup deleted"
+        }
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+
 };
