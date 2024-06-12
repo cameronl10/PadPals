a2911768571d29152f28b1f57b5bb429b5a1893d
jzhong16 <jooleyaz@gmail.com>
2024-06-11 20:48:54 -0700

    added wishgroup scaffolding

--- src/wishGroup.ts
+++ src/wishGroup.ts
@@ -1,1 +1,29 @@
 import Pool from '../config/dbConnect';
+
+interface WishGroup {
+    groupid: String
+    title: String
+    houseid: String
+};
+
+
+export const typeDefs = `#graphql
+    type WishGroup{
+        groupid: String
+        title: String
+        houseid: String
+    }
+    type Mutation {
+    
+    }
+    type Query{
+
+    }
+`;
+
+
+
+
+export const resolvers = {
+    
+};
