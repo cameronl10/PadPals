4b5e8380dd8add4b1187730d506a08b9a4c3edaf
Erik Lin <48135425+eriklsy@users.noreply.github.com>
2024-06-11 22:26:11 -0700

    Added get, and edit Title and edit color functionality (#12)
    Co-authored-by: Kangbin (Jay) Park <jaybinpark@gmail.com>

--- src/wishGroup.ts
+++ src/wishGroup.ts
@@ -1,16 +1,13 @@
 import Pool from '../config/dbConnect';
 
 interface WishGroup {
-delete
     title: String
     houseid: String
     color: String
 
 };
 
-
 export const typeDefs = `#graphql
-delete
     input WishGroupInput{
         title: String!
         houseid: String!
@@ -25,33 +22,80 @@ delete
         houseid: String
         color: String
     }
+    type WishGroup {
+        color: String
+     }
     type Mutation {
+
+        editGroupTitle(houseID: String!, title: String!, updatedTitle: String!): WishGroup
+        editGroupColor(houseID: String!, title: String!, updatedColor: String!): WishGroup
         createWishGroup(wishgroup: WishGroupInput!): WishGroup
         deleteWishGroup(title: String, houseid: String): WishGroup
-
+    }
+    type Query {
+        getGroup(houseID: String!, title: String!): WishGroup
     }
 `;
 
 
-
-
 export const resolvers = {
-delete
+    Query: {
+        getGroup: async (_: any, {houseID, title,}: any) => {
+            return await getWishGroup(houseID, title);
+        }
+    },
     Mutation: {
+        editGroupTitle: async (_: any, { houseID, title, updatedTitle }: any) => {
+            return await editWishGroupTitle( houseID, title, updatedTitle );
+        },
+        editGroupColor: async (_: any, { houseID, title, updatedColor }: any) => {
+            return await editWishGroupColor( houseID, title, updatedColor );
+        },
         createWishGroup: async (_: any, { wishgroup }: any) => {
             return await CreateWishGroup(wishgroup);
         },
         deleteWishGroup: async (_: any, { title, houseid }: any) => {
             return await DeleteWishGroup(title, houseid);
-        }
-    },
+    }
 };
+
+//Given houseID and title, update the title of the wishgroup
+async function editWishGroupTitle( houseID: String , title: String, updatedTitle: String) {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query(`UPDATE wishgroup SET title = $1 WHERE houseID = $2 AND title = $3 RETURNING *`, [updatedTitle, houseID, title]);
+        return result.rows[0];
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+}
+
+//Given houseID and title, update the color of the wishgroup
+async function editWishGroupColor( houseID: String , title: String, updatedColor: String  ) {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query(`UPDATE wishgroup SET color = $1 WHERE houseID = $2 AND title = $3 RETURNING *`, [updatedColor, houseID, title]);
+        return result.rows[0];
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+}
 
 async function CreateWishGroup(wishgroup: WishGroup) {
     const client = await Pool.connect();
     try {
         const result = await client.query('INSERT INTO wishgroup (title, houseid, color) VALUES($1, $2, $3) RETURNING *',
             [wishgroup.title, wishgroup.houseid, wishgroup.color]);
+      
+//Given houseID and title, return the wishgroup
+async function getWishGroup(houseID: String, title: String): Promise<WishGroup> {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query('SELECT * FROM wishgroup WHERE houseid = $1 AND title = $2', [houseID, title]);
         return result.rows[0];
     } catch (err) {
         console.log(err);
@@ -73,5 +117,4 @@ async function DeleteWishGroup(title: String, houseid: String) {
     } finally {
         client.release();
     }
-
 };
