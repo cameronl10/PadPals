bb6c9138e50407ef44e7dff60f9ba2613b8fc611
Cameron Lee <44252100+cameronl10@users.noreply.github.com>
2024-06-18 19:37:13 -0700

    refactored schemas and resolvers (#15)
    Co-authored-by: Jay Park <jaybinpark@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,43 @@
-import {ApolloServer} from '@apollo/server';
-import {startStandaloneServer} from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server'; // Corrected import
+import { startStandaloneServer } from '@apollo/server/standalone';
 import 'dotenv/config';
 import _ from 'lodash';
 import Pool from '../config/dbConnect';
-import { 
-    typeDefs as wishType,
-    resolvers as wishResolvers
-} from './wish';
-import {
-    typeDefs as wishGroupType,
-    resolvers as wishGroupResolvers
-} from './wishGroup';
 
-
-
-const typeDefs = `#graphql
-    type User{
-        name: String
-        userid: String
-    }
-
-    type Query{
-        users: [User]
-    }
-`;
-
-
-
-const resolvers = {
-    Query: {
-        users: testConnect
-    }
-}
+import resolvers from './resolvers/merger';
+import typeDefs from './schemas/merger';
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-    typeDefs : [typeDefs, wishType, wishGroupType],
-    resolvers : _.merge(wishResolvers, resolvers, wishGroupResolvers),
-  });
-  
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
-  async function testConnect(){
-    const client = await Pool.connect();
-    try {
-        const client = await Pool.connect();
-        console.log("connectecd to DB")
-    }catch(err){
-        console.log(err);
-    } finally {
-        client.release();
-    }
-  }
+  typeDefs,
+  resolvers,
+});
 
-  console.log(`🚀  Server ready at: ${url}`);
-  testConnect();
+async function startServer() {
+  console.log(typeDefs);
+  console.log(resolvers);
+  try {
+    // Starting the Apollo Server
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: 4000 },
+    });
+    console.log(`🚀 Server ready at: ${url}`);
+    await testConnect(); // Ensure DB connection after server starts
+  } catch (error) {
+    console.error('Error starting server or connecting to DB:', error);
+  }
+}
+
+async function testConnect() {
+  const client = await Pool.connect();
+  try {
+    console.log("Connected to DB");
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.release();
+  }
+}
+
+startServer();
