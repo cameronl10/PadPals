be9ddfffd0b71ea91febe887a3f71d75212e44af
Cameron Lee <44252100+cameronl10@users.noreply.github.com>
2024-06-18 17:44:38 -0700

    fixed deletion (#13)

--- src/wishGroup.ts
+++ src/wishGroup.ts
@@ -34,21 +34,70 @@ export const typeDefs = `#graphql
 
 
 export const resolvers = {
+    Query: {
+        getGroup: async (_: any, { houseID, title, }: any) => {
+            return await getWishGroup(houseID, title);
+        }
+    },
     Mutation: {
+        editGroupTitle: async (_: any, { houseID, title, updatedTitle }: any) => {
+            return await editWishGroupTitle(houseID, title, updatedTitle);
+        },
+        editGroupColor: async (_: any, { houseID, title, updatedColor }: any) => {
+            return await editWishGroupColor(houseID, title, updatedColor);
+        },
         createWishGroup: async (_: any, { wishgroup }: any) => {
             return await CreateWishGroup(wishgroup);
         },
         deleteWishGroup: async (_: any, { title, houseid }: any) => {
             return await DeleteWishGroup(title, houseid);
-        }
-    },
+        },
+    }
 };
+
+//Given houseID and title, update the title of the wishgroup
+async function editWishGroupTitle(houseID: String, title: String, updatedTitle: String) {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query(`UPDATE wishgroup SET title = $1 WHERE houseID = $2 AND title = $3 RETURNING *`, [updatedTitle, houseID, title]);
+        return result.rows[0];
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+}
+
+//Given houseID and title, update the color of the wishgroup
+async function editWishGroupColor(houseID: String, title: String, updatedColor: String) {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query(`UPDATE wishgroup SET color = $1 WHERE houseID = $2 AND title = $3 RETURNING *`, [updatedColor, houseID, title]);
+        return result.rows[0];
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+}
 
 async function CreateWishGroup(wishgroup: WishGroup) {
     const client = await Pool.connect();
     try {
         const result = await client.query('INSERT INTO wishgroup (title, houseid, color) VALUES($1, $2, $3) RETURNING *',
-            [wishgroup.title, wishgroup.houseid, wishgroup.color]);
+            [wishgroup.title, wishgroup.houseid, wishgroup.color])
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+};
+
+//Given houseID and title, return the wishgroup
+async function getWishGroup(houseID: String, title: String): Promise<WishGroup> {
+    const client = await Pool.connect();
+    try {
+        const result = await client.query('SELECT * FROM wishgroup WHERE houseid = $1 AND title = $2', [houseID, title]);
         return result.rows[0];
     } catch (err) {
         console.log(err);
