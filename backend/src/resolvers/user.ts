701e1e6df2897541de4cbc94753f16040c645b83
Kangbin (Jay) Park <jaybinpark@gmail.com>
2024-07-01 08:39:35 -0700

    created user edit field function (#33)

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -12,7 +12,39 @@ export const resolvers = {
     Query: {
     },
     Mutation: {
+        editUserFields: async(_: any, { user } : any): Promise<void> => {
+            return await EditUser(user);
+        }
     }
 };
+
+async function EditUser(user: Partial<User>): Promise<void> {
+    const client = await Pool.connect();
+    try {
+        let query = 'UPDATE account SET ';
+        let values = [];
+        let index = 1;
+
+        for (let key in user) {
+            if (user[key] !== undefined && key !== 'userid') {
+                query += `${key} = $${index}, `;
+                values.push(user[key]);
+                index++
+            }
+        }
+
+        query = query.slice(0, -2);
+
+        query += ` WHERE userid = $${index} RETURNING *`;
+        values.push(user.userid);
+
+        const result = await client.query(query, values);
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+}
 
 export default resolvers;
