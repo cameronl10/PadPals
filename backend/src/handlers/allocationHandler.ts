7ff4fda5d19f19aa72fd8026bd6d4bd4996c485c
Cameron Lee <44252100+cameronl10@users.noreply.github.com>
2024-07-09 18:58:15 -0700

    changed Number to number for primitives

--- backend/src/handlers/allocationHandler.ts
+++ backend/src/handlers/allocationHandler.ts
@@ -76,7 +76,7 @@ async function deleteAllocation(billid: String, userid: String): Promise<void> {
 };
 
 // Owed user is the user that is owed money, not the user that owes money
-async function getAllocationOwed(userId: String, owedUserid: String): Promise<Number> {
+async function getAllocationOwed(userId: String, owedUserid: String): Promise<number> {
     const client = await Pool.connect();
     try {
         // Left join on the bill with allocation table to get the total amount owed
