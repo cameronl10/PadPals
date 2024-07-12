b5da730f258636e1c3dfe9f5c80ec6fcd5061912
Cameron Lee <44252100+cameronl10@users.noreply.github.com>
2024-07-12 12:27:22 -0700

    added logout (#58)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,10 +43,11 @@ const server = new ApolloServer({
 await server.start();
 
 app.use('/graphql', expressMiddleware(server, {
-  context: async ({ req }: { req: any }) => {
+  context: async ({ req }: { req: any})=> {
     checkSession(req);
     return {
-      session: req.session
+      session: req.session,
+      req
     }
   },
 }
