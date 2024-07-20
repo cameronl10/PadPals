9596deffd46ec2e6b5869096cc3a9c1fa7cbc2fc
jzhong16 <jooleyaz@gmail.com>
2024-07-20 11:39:32 -0700

    uninstalled cva, removed test count :((

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -3,12 +3,6 @@ import { Text, View } from "react-native";
 import { Button } from "../components/ui/button";
 
 export default function Index() {
-  
-  //testing count
-  const [count, setCount] = useState(0);
-  function trackCount() {
-    setCount(prevCount => prevCount + 1);
-  }
 
   return (
     <View
@@ -19,8 +13,7 @@ export default function Index() {
       }}
     >
       <Button variant="bottom" title="bottom"/>
-      <Button variant="default" title="default" onPress={trackCount}/>
-      <Text>The count is now: {count}</Text>
+      <Button variant="default" title="default"/>
     </View>
   );
 }
