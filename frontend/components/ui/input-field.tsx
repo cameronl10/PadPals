14762d93c4a8ae17e0a7d9478bc003a74d432b9a
Erik Lin <erik.s.lin.2011@gmail.com>
2024-07-20 09:53:59 -0700

    Laid out structure

---
+++ frontend/components/ui/input-field.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, TextInput } from "react-native";
+
+const inputField = () => {
+  return (
+    <View>
+      <TextInput placeholder="Enter your name" />
+    </View>
+  );
+};
