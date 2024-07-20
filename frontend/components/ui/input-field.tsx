24df7dde4eceb826bc5c8556a52854d461828717
Erik Lin <erik.s.lin.2011@gmail.com>
2024-07-20 10:50:13 -0700

    Added Input-Field

--- frontend/components/ui/input-field.tsx
+++ frontend/components/ui/input-field.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { View, TextInput } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
 
-const inputField = () => {
+type InputFieldProps = {
+  type: "text" | "password";
+  containerStyle?: ViewStyle | ViewStyle[];
+  inputStyle?: TextStyle | TextStyle[];
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    paddingVertical: 5,
+  },
+});
+
+const InputField: React.FC<InputFieldProps> = ({
+  type,
+  containerStyle,
+  inputStyle,
+}) => {
   return (
-    <View>
-      <TextInput placeholder="Enter your name" />
+    <View style={[styles.container, containerStyle]}>
+      <TextInput
+        style={[styles.input, inputStyle]}
+        secureTextEntry={type === "password"}
+      />
     </View>
   );
 };
+
+export default InputField;
