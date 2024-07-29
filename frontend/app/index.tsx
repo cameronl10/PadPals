0be52c10b465e39f59801c67778c00bb8727922e
cameron lee <cameron.lee@smus.ca>
2024-07-29 12:55:17 -0700

    get started page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { Text, View } from "react-native";
 import { Button } from "../components/ui/button";
 import { Link, router } from "expo-router";
-
+import { InputField } from "@/components/ui/input-field";
+import {useForm} from 'react-hook-form'
 export default function Index() {
+  const form = useForm();
+
   return (
     <View
       style={{
@@ -15,6 +18,12 @@ export default function Index() {
       <Button variant="bottom" title="bottom" onPress={() => { router.push("/joinCreate") }} />
       <Button variant="default" title="default" />
       <Link href={{ pathname: "getStarted" }}>Get Started!</Link>
+      <InputField
+       variant="controlled"
+       label="test"
+       control={form.control}
+       name="test"
+      />
     </View>
   );
 }
