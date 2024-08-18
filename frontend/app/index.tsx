7f2eece045a3a1120b66655e23a8ee0c768ed02e
Julia Zhong <115321977+jooleyaz@users.noreply.github.com>
2024-08-18 12:23:17 -0700

    Fixes (#91)
    * added function to pay off all allocations owed towards one person
    
    * fixed wish query
    
    * added fixes

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,19 +2,16 @@ import { View } from "react-native";
 import { Button } from "../components/ui/button";
 import { Link, router } from "expo-router";
 import styles from '@/styles/globalStyle';
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Index() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.viewContainer}>
-        <Button variant="bottom" title="bottom" onPress={() => { router.push("/joinCreate") }} />
-        <Button variant="default" title="default" />
-        <Link href={{ pathname: "getStarted" }}>Get Started!</Link>
-        <Link href={{ pathname: "profileSetup" }}>Profile Setup</Link>
-        <Link href={{ pathname: "loginPage" }}>Login Page</Link>
-        <Link href={{ pathname: "createHouse" }}>Create or Join a House</Link>
-      </View>
-    </GestureHandlerRootView>
+    <View style={styles.viewContainer}>
+      <Button variant="bottom" title="bottom" onPress={() => { router.push("/joinCreate") }} />
+      <Button variant="default" title="default" />
+      <Link href={{ pathname: "getStarted" }}>Get Started!</Link>
+      <Link href={{ pathname: "profileSetup" }}>Profile Setup</Link>
+      <Link href={{ pathname: "loginPage" }}>Login Page</Link>
+      <Link href={{ pathname: "createHouse" }}>Create or Join a House</Link>
+    </View>
   );
 }
