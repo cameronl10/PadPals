b7c63d8f6e0c0f6186285a9800e943138274beba
cameron lee <cameron.lee@smus.ca>
2024-08-18 12:38:47 -0700

    added navbar + tabs

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,28 +1,46 @@
 
 import { Tabs } from 'expo-router'
+import BillIcon from '@/assets/icons/billIcon.svg'
+import HomeIcon from '@/assets/icons/houseIcon.svg'
+import WishIcon from '@/assets/icons/wishIcon.svg'
 
 export default function Layout() {
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor:"white",
+                tabBarActiveTintColor: "white",
                 headerShown: false,
                 tabBarStyle: {
-                    backgroundColor: '#515151',
-                    height:65
+                    backgroundColor: '#515151'
                 },
             }}>
-            <Tabs.Screen 
-                name="bills" 
-                options={{ 
+            <Tabs.Screen
+                name="bills"
+                options={{
                     title: "Bills",
-                    tabBarIcon: ({color,size}) => {
-                        
-                    }
-                }} 
+                    tabBarIcon: ({ color, size }) => (
+                        <BillIcon name="bill" size={size} color={color} />
+                    )
+                }}
             />
-            <Tabs.Screen name="home" options={{ title: "Home" }} />
-            <Tabs.Screen name="wishes" options={{ title: "Wishes" }} />
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, size }) => (
+                        <HomeIcon name="home" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="wishes"
+                options={{
+                    title: "Wishes",
+                    tabBarIcon: ({ color, size }) => (
+                        <WishIcon name="wish" size={size} color={color} />
+                    )
+                }}
+            />
         </Tabs>
     );
 }
