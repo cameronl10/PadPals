671340ff5a7a6e31fb1a4f4afa29f834587fdac6
jzhong16 <jooleyaz@gmail.com>
2024-08-18 15:36:15 -0700

    changed colors on stylesheet to be consistent

--- frontend/styles/signUpStyle.js
+++ frontend/styles/signUpStyle.js
@@ -1,10 +1,10 @@
 import { StyleSheet } from 'react-native';
-import {default as c} from '@/styles/colors';
+import Colors from '@/styles/colors';
 
 export default StyleSheet.create({
     keyboardView: {
         flex: 1,
-        backgroundColor: c.PADPALS_WHITE,
+        backgroundColor: Colors.PADPALS_WHITE,
         paddingTop: 80,
         paddingHorizontal: 10
     },
@@ -14,7 +14,7 @@ export default StyleSheet.create({
         fontWeight: "bold"
     },
     info: {
-        color: c.PADPALS_GREY
+        color: Colors.PADPALS_GREY
     },
     container: {
         flex: 1,
@@ -44,8 +44,8 @@ export default StyleSheet.create({
         borderRadius: 12,
         width: "80%",
         marginBottom: 10,
-        borderColor: c.PADPALS_BLACK,
-        backgroundColor: c.PADPALS_WHITE,
+        borderColor: Colors.PADPALS_BLACK,
+        backgroundColor: Colors.PADPALS_WHITE,
     },
     profileIcon: {
         alignSelf: "center"
