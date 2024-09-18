26bb01e5fbdf84e72011afc9385a63d06b32ede2
jzhong16 <jooleyaz@gmail.com>
2024-09-17 20:51:55 -0700

    created wishgroup filter component

--- frontend/app/(tabs)/wishes.tsx
+++ frontend/app/(tabs)/wishes.tsx
@@ -1,15 +1,20 @@
 import TopBar from "@/components/ui/topBar"
-import { View, Text, SafeAreaView, StyleSheet, Platform, StatusBar} from "react-native"
+import { View, Text, SafeAreaView, StyleSheet, Platform, StatusBar } from "react-native"
 import Colors from "@/styles/colors";
+import FilterBubble from "@/components/ui/filter-bubble";
 
-export default function Wishes(){
-    return(
-        <View style = {styles.container}>
-            <SafeAreaView style = {styles.header}>
-                <TopBar Header="Wishes"/>
+export default function Wishes() {
+    return (
+        <View style={styles.container}>
+            <SafeAreaView style={styles.header}>
+                <TopBar Header="Wishes" />
             </SafeAreaView>
-            <View style = {styles.body}>    
-                <Text>Wishes</Text>
+            <View style={styles.body}>
+                <FilterBubble
+                    wishGroups={["Groceries", "Chores", "Actions", "Apple", "Carrot", "Potato", "Pie"]}
+                    style={styles.filter}>
+                </FilterBubble>
+                <Text>Wishes test</Text>
             </View>
         </View>
     )
@@ -21,10 +26,15 @@ const styles = StyleSheet.create({
         flex: 1
     },
     header: {
-        paddingTop: Platform.OS === "android" ?( StatusBar.currentHeight || 0) + 5: 0,
+        paddingTop: Platform.OS === "android" ? (StatusBar.currentHeight || 0) + 5 : 0,
     },
     body: {
         flex: 3,
         backgroundColor: Colors.PADPALS_WHITE,
+    },
+    filter: {
+        marginTop: 8,
+        marginBottom: 8,
+        marginLeft: 8
     }
 });
---
+++ frontend/components/ui/filter-bubble.tsx
@@ -0,0 +1,42 @@
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView, ViewStyle } from 'react-native';
+import Colors from '@/styles/colors';
+
+interface filterBubbleProps {
+    wishGroups: string[]
+    style: ViewStyle
+}
+
+const FilterBubble = (props: filterBubbleProps) => {
+    return (
+        <View style={props.style}>
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.scrollViewContent}>
+                    {props.wishGroups.map((wishGroup, index) => (
+                        <TouchableOpacity key={index} style={styles.bubble}>
+                            <Text style={styles.bubbleText}>{wishGroup}</Text>
+                        </TouchableOpacity>
+                    ))}
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    scrollViewContent: {
+        alignItems: 'flex-start',
+    },
+    bubble: {
+        backgroundColor: Colors.PADPALS_GREY,
+        borderRadius: 20,
+        padding: 8,
+        marginRight: 6,
+    },
+    bubbleText: {
+        fontSize: 12,
+        color: Colors.PADPALS_WHITE
+    },
+});
+
+export default FilterBubble;
