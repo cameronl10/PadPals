a00dc82b735103cb0fe3980895dbce2cdf23341b
jzhong16 <jooleyaz@gmail.com>
2024-10-20 12:42:25 -0700

    added topbar to each tab page and fixed scrollview thing

--- frontend/api/client.ts
+++ frontend/api/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 import { getSessionKey } from '@/managers/sessionManager';
-const API_URL = 'http://localhost:3000/graphql';
+const API_URL = 'http://18.220.131.95:3000/graphql';
 
 export const getGraphqlClient = async () => {
     const sessionKey = await getSessionKey();
--- frontend/styles/mainPageStyle.js
+++ frontend/styles/mainPageStyle.js
@@ -1,4 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
+import Colors from "@/styles/colors";
 
 export default StyleSheet.create({
+    header: {
+        paddingTop: Platform.OS === "android" ? (StatusBar.currentHeight || 0) + 5 : 0,
+    },
+    body: {
+        position: 'absolute',
+        top: 100,
+        zIndex: 10,
+        flex: 1,
+    },
+    container: {
+        backgroundColor: Colors.PADPALS_PRIMARY,
+        flex: 0.3
+    },
 });
